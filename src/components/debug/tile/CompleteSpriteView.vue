01f85a425e1bad60b4852e6615b465c3995dafef
o-y <zv@zv.wtf>
2022-05-18 15:08:51 +0100

    Create inital repository and start on debug views

---
+++ src/components/debug/tile/CompleteSpriteView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CompleteSpriteView extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import CompleteSpriteView from '@/components/debug/tile/CompleteSpriteView.vue';
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/debug/tile/completespriteview',
+    name: 'Home',
+    component: CompleteSpriteView
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
