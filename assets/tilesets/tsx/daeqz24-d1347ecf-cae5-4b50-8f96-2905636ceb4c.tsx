c4e99327b1526428642b0e7de3a00b16505fe8ae
o-y <zv@zv.wtf>
2022-07-27 18:27:31 +0100

    move netlify redirect rule to public

---
+++ assets/tilesets/tsx/daeqz24-d1347ecf-cae5-4b50-8f96-2905636ceb4c.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="daeqz24-d1347ecf-cae5-4b50-8f96-2905636ceb4c" tilewidth="48" tileheight="48" tilecount="256" columns="16">
+ <image source="../../../../../Documents/dev/realm_deprecated/branding/assets/pokemon/daeqz24-d1347ecf-cae5-4b50-8f96-2905636ceb4c.png" width="768" height="768"/>
+</tileset>
